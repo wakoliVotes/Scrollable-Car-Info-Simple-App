28c3beea9e1a1ae920b397a4b4e8b1981a0b697d
Danny-Votez <votesdw@yahoo.com>
2021-11-19 12:04:02 +0300

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.automaticautotracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
